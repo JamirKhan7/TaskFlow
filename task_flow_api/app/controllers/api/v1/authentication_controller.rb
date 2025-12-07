bcda632317db9c9734c61b6f7a77159482c2919e
JamirKhan7 <jamirkhan007@gmail.com>
2025-12-07 09:05:48 +0530

    login action

--- task_flow_api/app/controllers/api/v1/authentication_controller.rb
+++ task_flow_api/app/controllers/api/v1/authentication_controller.rb
@@ -2,5 +2,23 @@ class Api::V1::AuthenticationController < ApplicationController
   skip_before_action :authentication_request, only: [:login]
 
   def login
+    @user = User.find_by_email(params[:email])
+
+    if @user&.authenticate(params[:password])
+      token = JsonWebToken.encode({ user_id: @user.id })
+
+      render json: {
+        token: token,
+        user: {
+          id: @user.id,
+          email: @user.email,
+          name: "#{@user.first_name} #{@user.last_name}"
+        }
+      }, status: :ok
+    else
+      render json: {
+        error: "unauthorized"
+      }, status: :unauthorized
+    end
   end
 end
